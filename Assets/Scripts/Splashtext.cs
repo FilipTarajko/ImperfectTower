feb03a7ff29c08bae513e3700c02d0ae817d2dd2
Filip Tarajko <filiptarajko@gmail.com>
2021-02-17 14:03:26 +0100

    make splash text grow and shrink

--- Assets/Scripts/Splashtext.cs
+++ Assets/Scripts/Splashtext.cs
@@ -7,6 +7,9 @@ public class Splashtext : MonoBehaviour
 {
     [SerializeField] private TMP_Text splashdisplay;
     [SerializeField] private TMP_Text splashshadow;
+    public double scale;
+    public double change;
+
     string[] splashtexts = { "Minecraft's better", 
         "Also try Perfect Tower!",
         //2021 == bin 11111100101
@@ -24,5 +27,29 @@ public class Splashtext : MonoBehaviour
         int chosen = Random.Range(0, splashtexts.Length);
         splashdisplay.text = splashtexts[chosen];
         splashshadow.text = splashtexts[chosen];
+    }
+
+    private void Update()
+    {
+        if (scale > 1.04f)
+        {
+            change = -1;
+        }
+        else if (scale < 0.96f)
+        {
+            change = 1;
+        }
+
+        if (change == 1)
+        {
+            scale *= System.Math.Pow(1.4, Time.deltaTime);
+        }
+        else if (change == -1)
+        {
+            scale /= System.Math.Pow(1.4, Time.deltaTime);
+        }
+        
+        //scale = Random.Range(0.5f, 2f);
+        gameObject.transform.localScale = new Vector3 ((float)scale, (float)scale, (float)scale);
     }
 }
